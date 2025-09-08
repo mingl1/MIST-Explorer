432dae7b1640bc02e14ae03c64b5d21c5098acbb
mingl1 <linm1@bxscience.edu>
2025-09-08 14:18:41 -0400

    better progress

--- core/register.py
+++ core/register.py
@@ -156,17 +156,18 @@ class Register(QThread):
                 if self._handle_cancel():
                     return
                 progress_update = int(((tile_n + 1) / len(inputs)) * 100)
-                self.progress.emit(
-                    progress_update,
-                    str(f"aligning tile {tile_n+1}/{len(inputs)}"),
-                )
+                
 
                 if tif_n == 0:
                     outputs.append(self.on_skip(tile_set))
                     continue
 
                 result = self.align_two_img_robust(*tile_set)  # align
-
+                transforms, ymin, xmin, radius, x, y, best_ncc = result
+                self.progress.emit(
+                    progress_update,
+                    str(f"aligning tile {tile_n+1}/{len(inputs)} - NCC: {best_ncc}"),
+                )
                 if result is None:
                     continue
                 outputs.append(result)
